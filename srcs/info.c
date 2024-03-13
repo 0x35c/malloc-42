a2f360c1bc9967d18bf670af753f9e1e9cb890e4
Unai Layus <ulayus@car9s1.42barcelona.com>
2024-03-13 12:48:06 +0100

    The blocks for TINY and SMALL should be working and formatted as it should show_alloc_mem() is pretty clean and complete

--- srcs/info.c
+++ srcs/info.c
@@ -1,20 +1,30 @@
 #include "../includes/malloc.h"
 
+size_t get_nb_blocks(Block *head) {
+	size_t size = 0;
+	for (Block *it = head; it != NULL; it = it->next) {
+		/* printf("iter addr %p\n", it); */
+		size++;
+	}
+	return (size);
+}
+
 void show_alloc_mem(void)
 {
-	for (Zone *zone_it = zones; zone_it->next != NULL; zone_it = zone_it->next) {
+	for (Zone *zone_it = zones; zone_it != NULL; zone_it = zone_it->next) {
 		if (zone_it->type == TINY)
-			ft_printf("TINY : %p\n", zone_it);
+			ft_printf("TINY : %p with %u blocks\n", zone_it, get_nb_blocks(zone_it->head));
 		else if (zone_it->type == SMALL)
-			ft_printf("SMALL : %p\n", zone_it);
+			ft_printf("SMALL : %p with %u blocks\n", zone_it, get_nb_blocks(zone_it->head));
 		else if (zone_it->type == LARGE)
 			ft_printf("LARGE : %p\n", zone_it);
-		for (Block *block_it = zone_it->head; block_it->next != NULL; block_it = block_it->next) {
+		for (Block *block_it = zone_it->head; block_it != NULL; block_it = block_it->next) {
 			ft_printf("%p - %p : %u bytes ", block_it, (size_t)block_it + block_it->size, block_it->size);
 			if (block_it->in_use == false)
 				ft_printf("(not in_use)\n");
 			else
 				ft_printf("(in_use)\n");
 		}
+		ft_printf("------------------------------\n");
 	}
 }
--- srcs/init.c
+++ srcs/init.c
@@ -6,16 +6,22 @@ Zone *zones;
 
 static void init_blocks(Zone *zone, size_t nb_blocks, size_t block_size)
 {
-	zone->head = (Block *)(zone + sizeof(Zone));
+	const size_t zone_size = (size_t)zone + nb_blocks * block_size + sizeof(Zone);
+	zone->head = (Block *)((size_t)zone + sizeof(Zone));
 	Block *iter = zone->head;
-	while ((uintptr_t)iter + block_size < (uintptr_t)zone + nb_blocks * block_size) {
-		printf("zone %p - iter %p - block_size %ld\n", zone, iter, block_size / 32);
-		iter->next = iter + (block_size / 32);
-		iter->next->size = (block_size / 32) - sizeof(Block);
-		iter->next->in_use = false;
-		printf("zone %p - iter next %p - block_size %ld\n", zone, iter->next, block_size / 32);
+	do {
+		iter->size = block_size - sizeof(Block);
+		iter->in_use = false;
+		iter->next = (Block *)((size_t)iter + block_size);
 		iter = iter->next;
-	}
+		/* printf("iter size_t: %ld - iter ptr: %p\n", (size_t)iter, iter); */
+		/* printf("zone %p - iter %p - block_size %ld\n", zone, iter, block_size); */
+		/* printf("sum of iter + block_size: %p\n", iter + block_size); */
+		/* printf("zone %p - iter->next %p - block_size %ld\n", zone, iter + block_size,  block_size); */
+		/* printf("New block here %p\n", iter); */
+	} while ((size_t)iter + block_size <= zone_size);
+	Block *prev = (Block *)((size_t)iter - block_size);
+	prev->next = NULL;
 }
 
 int init_allocator(void)
@@ -26,15 +32,15 @@ int init_allocator(void)
 
 	/* Size of each block (in bytes)
 	 * /!\ This is not the real size, the real size is 
-	 * of X_size - sizeof(Zone) 
-	 * Which is the remaining size of the block we got
-	 * with mmap once we added the metadata
+	 * of X_size - sizeof(Block) 
+	 * Which is the remaining size of the block
+	 * once we added the metadata
 	 */
 	const size_t tiny_size = getpagesize() * PAGES_TINY;
 	const size_t small_size = getpagesize() * PAGES_SMALL;
 
 	// Init TINY zones with NB_BLOCKS_INIT blocks of 4 pages 
-	Zone *tiny = (Zone *)mmap(NULL, tiny_size * NB_BLOCKS_INIT,
+	Zone *tiny = (Zone *)mmap(NULL, tiny_size * NB_BLOCKS_INIT + sizeof(Zone),
 								PROT_READ | PROT_WRITE,
 								MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	if (tiny == NULL) {
@@ -47,7 +53,7 @@ int init_allocator(void)
 	init_blocks(tiny, NB_BLOCKS_INIT, tiny_size);
 
 	// Init SMALL zones with NB_BLOCKS_INIT blocks of 16 pages, and set the TINY to point towards SMALL
-	Zone *small = (Zone *)mmap(NULL, small_size * NB_BLOCKS_INIT,
+	Zone *small = (Zone *)mmap(NULL, small_size * NB_BLOCKS_INIT + sizeof(Zone),
 								PROT_READ | PROT_WRITE,
 								MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	if (small == NULL) {
